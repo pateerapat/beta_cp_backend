8e1b19e3160de6446abe792f4b7e0e5839013cd7
Teerapat <teerapat.akson@gmail.com>
2023-05-15 02:10:32 +0700

    Fix staticfile

--- beta_cp_backend/settings.py
+++ beta_cp_backend/settings.py
@@ -26,10 +26,7 @@ SECRET_KEY = 'django-insecure-a&^n$*=pccj^$8g$yl#z&62b3^ssjdyff7sbw_80@xe)53@@=q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '*',
-    'https://betacpbackend-production.up.railway.app',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -156,7 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
